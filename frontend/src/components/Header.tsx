98ff1add4d7a5cbe56a0b8c416c886edee8a391c
Alok <alokkhalkho65@gmail.com>
2024-08-29 23:46:06 +0530

    Login button for desktop screen

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import MobileNav from './MobileNav'
+import MainNav from './MainNav'
 
 export default function Header() {
     return (
@@ -8,6 +9,9 @@ export default function Header() {
                 <Link to="/" className="text-3xl font-bold tracking-tight text-orange-500">FoodGo</Link>
                 <div className='md:hidden'>
                     <MobileNav />
+                </div>
+                <div className='hidden md:block'>
+                    <MainNav />
                 </div>
 
             </div>
