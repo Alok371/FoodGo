bb24bc241c4e85cbd4019ec91b2587bfdbd34a27
Alok <alokkhalkho65@gmail.com>
2024-09-01 00:01:46 +0530

    fix some errors for user profile form

--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -2,8 +2,6 @@ import UserProfileForm from '@/forms/user-profile-form/UserProfileForm'
 
 export default function UserProfilePage() {
     return (
-        <UserProfileForm onSave={function (userProfileData: { name: string; address: string; city: string; country: string; email?: string | undefined }): void {
-            throw new Error('Function not implemented.')
-        }} isLoading={undefined} />
+        <UserProfileForm />
     )
 }
