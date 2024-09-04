6c2b908a476d61220f83e4e75417efc2fc7610db
Alok <alokkhalkho65@gmail.com>
2024-09-04 23:22:58 +0530

    created restaurant api and form

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,14 +4,15 @@ import "dotenv/config"
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
 import { v2 as cloudinary } from "cloudinary";
+import myRestaurantRoute from "./routes/MyRestaurantRoute"
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("Connected to database"))
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    API_KEY: process.env.CLOUDINARY_API_KEY,
+    api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
-})
+});
 
 const app = express();
 
@@ -23,6 +24,7 @@ app.get("/health", async (req: Request, res: Response) => {
 })
 
 app.use("/api/my/user", myUserRoute)
+app.use("/api/my/restaurant", myRestaurantRoute)
 
 app.listen(7000, () => {
     console.log("Server started at Localhost:7000")
